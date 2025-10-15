47ef6d722435a6289af25bc22ea85e0c62dd557c
Luis Leal <luisrrleal@gmail.com>
2025-10-15 15:13:55 -0600

    Fix registration route - add name to POST route

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,8 @@ Route::get('/', function () {
 Route::middleware('guest')->group(function () {
     Route::get('/login', [WebAuthController::class, 'showLogin'])->name('login');
     Route::post('/login', [WebAuthController::class, 'login'])->name('auth.login');
-    Route::get('/register', [WebAuthController::class, 'showRegister'])->name('auth.register');
-    Route::post('/register', [WebAuthController::class, 'register']);
+    Route::get('/register', [WebAuthController::class, 'showRegister'])->name('register');
+    Route::post('/register', [WebAuthController::class, 'register'])->name('auth.register');
 });
 
 Route::post('/logout', [WebAuthController::class, 'logout'])
