c4dc13d3a83a18497e68b498c1b8f61d0d000f30
Luis Leal <luisrrleal@gmail.com>
2025-10-15 21:32:19 -0600

    Add rental controller

--- app/Http/Controllers/RentalController.php
+++ app/Http/Controllers/RentalController.php
@@ -10,7 +10,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
-
 class RentalController extends Controller
 {
     /**
